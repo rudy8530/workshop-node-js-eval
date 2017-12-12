8b2690602207a7b8cb72284e357fb6bacb250831
Schoepfer <rudy.schoepfer@hotmail.fr>
2017-12-12 16:54:51 +0100

    Exercise 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,12 @@
 exports.run = function(input) {
-  console.log("Exercise #5");
+  var fs = require('fs');
+    var contenu,rightcontenu;
+    contenu = fs.readFileSync(input, "UTF-8");
+    rightcontenu = contenu.split("\r\n")
+    var somme=0;
+    for(var i=0; i<rightcontenu.length;i++){
+      var value = parseInt(rightcontenu[i], 10);
+      somme = somme +value;
+    }
+    return somme;
 };
